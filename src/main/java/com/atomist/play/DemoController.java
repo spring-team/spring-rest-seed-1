20fc0adbfb3765ee05e8bd1b606d7f33a0e819d0
Christian Dupuis <cd@atomist.com>
2018-10-08 11:44:35 +0200

    Update DemoController.java

--- src/main/java/com/atomist/play/DemoController.java
+++ src/main/java/com/atomist/play/DemoController.java
@@ -1,7 +1,7 @@
 package com.atomist.play;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -9,12 +9,12 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 class DemoController {
 
-    @RequestMapping(method = GET, path = "/")
+    @GetMapping("/")
     public String root() {
         return "Hello Kubernetes demo on Wed! I'm served by " + getClass().getName();
     }
 
-    @RequestMapping(method = GET, path = "hello/{name}")
+    @GetMapping("hello/{name}")
     public String person(@PathVariable String name) {
         return "Hello " + name + "!";
     }
